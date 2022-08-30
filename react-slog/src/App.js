a4fdd0e6a352da2f8ba3fdb7e91280b69d1c0507
sihem <mhadhbisihem0@gmail.com>
2022-08-30 22:23:29 +0200

    create react app

---
+++ react-slog/src/App.js
@@ -0,0 +1,56 @@
+import logo from "./fb.svg";
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/css/bootstrap.css";
+//import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+
+import Container from "react-bootstrap/Container";
+
+//import * as React from "react";
+function App() {
+  return (
+    <Container>
+      <div className="App">
+        <header className="App-header">
+          <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+              <Form.Check type="checkbox" label="Check me out" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Contact <code>With Others</code> and enjoy.
+          </p>
+          <a
+            className="App-link"
+            href="https://www.facebook.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Welcome
+          </a>
+        </header>
+      </div>
+    </Container>
+  );
+}
+
+export default App;
